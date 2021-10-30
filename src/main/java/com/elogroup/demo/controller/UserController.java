5f97485352dc42e46bbf2b901ff7f0e75eaec927
Celso Bertolotto <celsonyl@hotmail.com>
2021-10-30 15:08:57 -0300

    add annotation @CrossOrigin in createUser

--- src/main/java/com/elogroup/demo/controller/UserController.java
+++ src/main/java/com/elogroup/demo/controller/UserController.java
@@ -6,10 +6,7 @@ import com.elogroup.demo.translator.UserMapperImpl;
 import com.elogroup.demo.usercase.CreateUserUsecase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
@@ -23,6 +20,7 @@ public class UserController {
     private CreateUserUsecase createUserUsecase;
 
     @PostMapping
+    @CrossOrigin
     public ResponseEntity<UserResponse> createUser(@RequestBody @Valid UserRequest userRequest, UriComponentsBuilder uriComponentsBuilder) {
         var user = createUserUsecase.execute(new UserMapperImpl().userRequestToDomain(userRequest));
         URI uri = uriComponentsBuilder.path("user/{id}").buildAndExpand(user.getId()).toUri();
