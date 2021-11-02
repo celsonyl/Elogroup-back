fef5a8061a58e186270ee51dae9b1659e7b21393
Celso Bertolotto <celsonyl@hotmail.com>
2021-11-01 21:14:27 -0300

    cross origin

--- src/main/java/com/elogroup/demo/controller/LeadController.java
+++ src/main/java/com/elogroup/demo/controller/LeadController.java
@@ -6,10 +6,7 @@ import com.elogroup.demo.translator.LeadMapperImpl;
 import com.elogroup.demo.usercase.CreateLeadUsecase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
@@ -23,6 +20,7 @@ public class LeadController {
     private CreateLeadUsecase createLeadUsecase;
 
     @PostMapping
+    @CrossOrigin
     public ResponseEntity<LeadResponse> createLead(@RequestBody @Valid LeadRequest leadRequest, UriComponentsBuilder uriComponentsBuilder) {
         var leadSaved = createLeadUsecase.execute(new LeadMapperImpl().leadRequestToDomain(leadRequest));
         URI uri = uriComponentsBuilder.path("lead/{id}").buildAndExpand(leadSaved.getId()).toUri();
